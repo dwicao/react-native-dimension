48cfcff01488427022cbdeb863628101997af4d5
dwicao <lutfiandwica@gmail.com>
2017-04-19 10:56:11 +0700

    fix typo on test

--- __tests__/src/utils/index.js
+++ __tests__/src/utils/index.js
@@ -2,21 +2,21 @@ import handleSize from '../../../src/utils';
 
 describe('utils', () => {
   describe('handleSize', () => {
-    it('return zero if given value is below zero', () => {
+    it('return 0 if given value is below 0', () => {
       const value = -45;
       const expected = 0;
 
       expect(handleSize(value)).toBe(expected);
     });
 
-    it('return one hundred if given value is above one hundreds', () => {
+    it('return 1 if given value is above 100', () => {
       const value = 4000;
       const expected = 1;
 
       expect(handleSize(value)).toBe(expected);
     });
 
-    it('return a number with correct comma from given number', () => {
+    it('return a number with correct decimal from given number', () => {
       const value = 45;
       const expected = 0.45;
 
